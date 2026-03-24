6109aae5aa8cbdd106115db00ba8b86d15af7d17
Kuznetsov Denis <163781660+seeuagony@users.noreply.github.com>
2026-03-24 09:00:14 +0100

    Refactor main function and add helper functions
    Refactor main function to improve readability and modularity.

--- task2.cpp
+++ task2.cpp
@@ -3,6 +3,19 @@
 #include <algorithm>
 #include <iterator>
 
+std::vector<int> insertMinusOdin(std::vector<int> v);
+void printVector(const std::vector<int>& v);
+
+int main() {
+    std::vector<int> v = {3, 7, 1, 9, 4};
+    std::cout << "До преобразований:  ";
+    printVector(v);
+    auto result = insertMinusOdin(v);
+    std::cout << "После преобразований  : ";
+    printVector(result);
+    return 0;
+}
+
 std::vector<int> insertMinusOdin(std::vector<int> v) {
     auto it = v.begin();
     while (it != v.end()) {
@@ -17,15 +30,4 @@ void printVector(const std::vector<int>& v) {
     std::copy(v.begin(), v.end(),
               std::ostream_iterator<int>(std::cout, " "));
     std::cout << '\n';
-}
-
-int main() {
-    std::vector<int> v = {3, 7, 1, 9, 4};
-    std::cout << "До преобразований:  ";
-    printVector(v);
-    auto result = insertMinusOdin(v);
-    std::cout << "После преобразований  : ";
-    printVector(result);
-
-    return 0;
 }
