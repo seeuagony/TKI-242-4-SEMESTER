7a6b076b1edc92e953248c806d11b1d053a2f81d
Kuznetsov Denis <163781660+seeuagony@users.noreply.github.com>
2026-03-24 09:01:20 +0100

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
-  #include <vector>
-  #include <set>
-  #include <algorithm>
+#include <vector>
+#include <set>
+#include <algorithm>
 
-  struct PrintWithCount {
-      const std::multiset<int>& m;
+struct PrintWithCount {
+    const std::multiset<int>& m;
+    explicit PrintWithCount(const std::multiset<int>& ms);
+    void operator()(int val) const;
+};
 
-      explicit PrintWithCount(const std::multiset<int>& ms) : m(ms) {}
+int main() {
+    std::vector<int> v = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+    std::multiset<int> M(v.begin(), v.end());
+    std::set<int> S(v.begin(), v.end());
+    std::for_each(S.begin(), S.end(), PrintWithCount(M));
+    std::cout << '\n';
+    return 0;
+}
 
-      void operator()(int val) const {
-          std::cout << val << "(" << m.count(val) << ") ";
-      }
-  };
+PrintWithCount::PrintWithCount(const std::multiset<int>& ms) : m(ms) {}
 
-  int main() {
-      std::vector<int> v = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
-      // это все элементы которые дублируются
-      std::multiset<int> M(v.begin(), v.end());
-      // это только уникальные элементы
-      std::set<int>S(v.begin(), v.end());
-      std::for_each(S.begin(), S.end(), PrintWithCount(M));
-      std::cout << '\n';
-
-      return 0;
-  }
+void PrintWithCount::operator()(int val) const {
+    std::cout << val << "(" << m.count(val) << ") ";
+}
